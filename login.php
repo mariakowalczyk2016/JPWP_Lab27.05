bc9908bc7693e2aa72bbc5cb2f40c1929f5b9468
mariakowalczyk2016 <44197788+mariakowalczyk2016@users.noreply.github.com>
2020-05-27 10:57:42 +0200

    rozwiązanie

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+$servername="localhost";
+$mysql_user="root";
+$mysql_pass="";
+$dbname="JPWP";
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $conn=mysqli_connect($servername, $mysql_user, $mysql_pass, $dbname);
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $query="SELECT `name` FROM `registration` WHERE( email=? AND password=?)";
+    $stmt=mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, 'ss', $email, $password);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_bind_result($stmt, $name);
+    if(mysqli_stmt_fetch($stmt)){
+    echo 'Hello ' . $name;
+    }else{
+    echo("Login failed");
+    }
+    
+} else {
+    echo("Error in request method");
+}
